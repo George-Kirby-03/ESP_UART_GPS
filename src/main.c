3f13f1a494c847b3c4fdedd4cc5f11a4926b78f2
George Kirby <94544219+Kirbyinmars@users.noreply.github.com>
2025-04-17 20:40:08 +0100

    Plain mnea

--- src/main.c
+++ src/main.c
@@ -24,32 +24,29 @@ uart_config_t uart_config_gps = {
 void app_main() {
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config_gps)); // Install UART driver
     ESP_ERROR_CHECK(uart_set_pin(uart_num, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE)); // Set UART pins
-    const int uart_buffer_size = 600; // Buffer size
+    const int uart_buffer_size = 200; // Buffer size
     QueueHandle_t uart_queue; // UART queue
     ESP_ERROR_CHECK(uart_driver_install(uart_num, uart_buffer_size, 0, 10, &uart_queue, 0)); // Install UART driver
-    uint8_t data[600]; // Buffer for received data
+    uint8_t data[100]; // Buffer for received data
     uart_event_t event;
     while (1) {
         // Wait for a UART event to occur
-        if (xQueueReceive(uart_queue, &event, 100/portTICK_PERIOD_MS)) {
-            switch (event.type) {
-                case UART_DATA:
-                  int len = uart_read_bytes(uart_num, data, sizeof(data), 1000/ portTICK_PERIOD_MS);
+        int len = uart_read_bytes(uart_num, data, sizeof(data), 10/ portTICK_PERIOD_MS);
                    if (len > 0) {
+                       
                        data[len] = '\0'; // Null-terminate the string
                        printf("%s", data); // Log the received data
                    }
-                   else if (len == -1) {
-                       ESP_LOGE("UART", "Error reading data");
-                   }
-                   else {
-                       ESP_LOGI("UART", "No data received");
-                   }
+        if (xQueueReceive(uart_queue, &event, 0)) {
+            switch (event.type) {
+                case UART_DATA:
                     break;
                 case UART_FIFO_OVF:
                 ESP_LOGE("UART", "Error fifo is full!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                uart_flush(uart_num); // Flush the UART buffer
                     break;
                 default: 
+                    ESP_LOGI("UART", "Unhandled event type: %d", event.type);
                     break;        
             }
         }
